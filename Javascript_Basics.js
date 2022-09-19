db29ec269ec00faddff2b2fb25810bb204edb2a0
nzscode <93976904+nzscode@users.noreply.github.com>
2022-09-19 10:16:14 -0400

    Update Javascript_Basics.js

--- Javascript_Basics.js
+++ Javascript_Basics.js
@@ -1,5 +1,14 @@
-function sum(a,b){
-    return a+b;
-}
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
 
-console.log(sum(2,3));
+
+// const half = (stats.max + stats.min)/2;
+
+const {max, min} = stats;
+  console.log(half/2);
