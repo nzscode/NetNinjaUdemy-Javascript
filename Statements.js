07afa91e4f398a6dd71972bc1e1c719b6d5e7203
nzscode <93976904+nzscode@users.noreply.github.com>
2022-09-19 10:16:07 -0400

    Update Statements.js

---
+++ Statements.js
@@ -0,0 +1,8 @@
+//For       defines a code block that is executed as long as a condition is true.
+//Loop (iterate over) a code block five times:
+
+let stringArray = ["You", "Are", "A", "Wizard", "Harry"];
+for (let i=0; i<5; i++){
+    console.log(i);
+}
+
