4bc1c9036dcaee1b29241506b40e63478d7d799d
nzscode <93976904+nzscode@users.noreply.github.com>
2022-11-02 17:18:10 -0400

    Create ToDoList.js

---
+++ Project3_ToDoList/ToDoList.js
@@ -0,0 +1,40 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+// const generateTemplate = function (){
+
+// }
+
+const generateTemplate = (todo) => {
+const html = `<li class="list-group-item d-flex justify-content-between align-items-center text-light">
+                <span>${todo}</span>
+                <i class="fa-solid fa-trash-can delete"></i>
+                </li>`;
+list.innerHTML+= html;
+};
+
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+//delete todos
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+})
+
+const filterTodos = (term) => {
+ 
+};
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim();
+    filterTodos(term);
+})
