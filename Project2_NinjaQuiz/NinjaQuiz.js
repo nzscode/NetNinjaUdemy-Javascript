bae224c04d5b0f81aab2d4c73554bfee7aa674df
nzscode <93976904+nzscode@users.noreply.github.com>
2022-11-02 17:17:59 -0400

    Update NinjaQuiz.js

--- Project2_NinjaQuiz/NinjaQuiz.js
+++ Project2_NinjaQuiz/NinjaQuiz.js
@@ -16,11 +16,30 @@ form.addEventListener('submit', (e) =>{
         }
     });
     //show result on page
-
+        scrollTo(0,0); //scrolls to  0,0 of page. window object
     // let resultSpan = result.querySelector('span');    
     // resultSpan.textContent = `${score}%`;
-
-    result.querySelector('span').textContent = `${score}%`;
+    
+    // moved to the timer function
+    // result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
-})
 
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 35);
+});
+
+// let i = 0;
+// const timer = setInterval(() => {
+//  console.log('hello');
+//  i++;
+//  if(i === 5){
+//  clearInterval(timer);
+//  }
+// }, 1000);
